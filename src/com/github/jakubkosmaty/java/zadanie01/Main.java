626239c01abfb6edecb8fc719eab04b9f5b7d3c3
Jakub Kosmaty <jacobkosmaty@gmail.com>
2020-10-22 12:40:51 +0200

    first init

---
+++ src/com/github/jakubkosmaty/java/zadanie01/Main.java
@@ -0,0 +1,17 @@
+package com.github.jakubkosmaty.java.zadanie01;
+
+public class Main {
+    public static void main(String[] args) {
+        DecoderAbstraction decoder = new Decoder();
+
+        decoder.decode("12");
+        decoder.decode("3");
+        decoder.decode("");
+        decoder.decode("4");
+        decoder.decode("a");
+        decoder.decode("b");
+        decoder.decode("cd");
+
+        System.out.println(decoder.getBuffer());
+    }
+}
